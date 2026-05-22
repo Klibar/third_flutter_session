ac03b8f703f5eda502d43e9fbdfa16addf6fb90b
Klibar <ma8964032@gmail.com>
2026-05-22 20:27:42 +0300

    finish

--- lib/screens/conestants.dart
+++ lib/screens/conestants.dart
@@ -19,7 +19,9 @@ class Category extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: 65,
       decoration: BoxDecoration(
+        boxShadow: [BoxShadow(color: Colors.black, blurRadius: 0.001)],
         color: bgColor,
         borderRadius: BorderRadius.circular(50),
       ),
