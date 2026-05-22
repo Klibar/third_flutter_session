cf310a8141c1e794fc2d2ebfed9025a9dbf0c6b4
Klibar <ma8964032@gmail.com>
2026-05-22 16:41:28 +0300

    finsh

--- lib/screens/conestants.dart
+++ lib/screens/conestants.dart
@@ -3,3 +3,51 @@ import 'package:flutter/material.dart';
 class MyColors {
   static var mainColor = Color(0xffff7622);
 }
+
+class Axiss {
+  static double imageWidth = 40;
+  static double imageHeight = 40;
+}
+
+class Category extends StatelessWidget {
+  const Category({
+    super.key,
+    required this.bgColor,
+    required this.imagePath,
+    required this.titel,
+    required this.hoverColor,
+  });
+  final int bgColor;
+  final Color hoverColor;
+  final String imagePath;
+  final String titel;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 130,
+      height: 60,
+      decoration: BoxDecoration(
+        color: Color(bgColor),
+        borderRadius: BorderRadius.circular(50),
+        boxShadow: [
+          BoxShadow(color: Colors.black, blurRadius: 1, spreadRadius: -1),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: .spaceAround,
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: hoverColor,
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Image.asset(imagePath, fit: BoxFit.contain),
+          ),
+          Text(titel, style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -6,189 +6,298 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(top: 50, right: 15, left: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(Icons.sort_outlined),
-                      style: IconButton.styleFrom(
-                        backgroundColor: Color(0xffecf0f4),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 50, right: 15, left: 15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.sort_outlined),
+                        style: IconButton.styleFrom(
+                          backgroundColor: Color(0xffecf0f4),
+                        ),
                       ),
+                      SizedBox(width: 15),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'DELIVER TO',
+                            style: TextStyle(
+                              color: MyColors.mainColor,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Text('Halal Lab office'),
+                              Icon(Icons.arrow_drop_down),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.shopping_bag_outlined),
+                    color: Colors.white,
+                    style: IconButton.styleFrom(
+                      backgroundColor: Color(0xff181c2e),
                     ),
-                    SizedBox(width: 15),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                children: [
+                  Text('Hey Halal,', style: TextStyle(fontSize: 19)),
+                  Text(
+                    'Good Afternoon',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Container(
+                    width: 300,
+                    height: 65,
+                    decoration: BoxDecoration(
+                      color: Color(0xffe9eef6),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
                       children: [
-                        Text(
-                          'DELIVER TO',
-                          style: TextStyle(
-                            color: MyColors.mainColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 12,
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 15),
+                          child: Icon(
+                            Icons.search,
+                            size: 35,
+                            color: Color(0xffbec1cf),
                           ),
                         ),
-                        Row(
-                          children: [
-                            Text('Halal Lab office'),
-                            Icon(Icons.arrow_drop_down),
-                          ],
+                        Text(
+                          'Search dishes, restaurants',
+                          style: TextStyle(
+                            color: Color(0xffbec1cf),
+                            fontSize: 17,
+                          ),
                         ),
                       ],
                     ),
-                  ],
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.shopping_bag_outlined),
-                  color: Colors.white,
-                  style: IconButton.styleFrom(
-                    backgroundColor: Color(0xff181c2e),
                   ),
                 ),
               ],
             ),
-          ),
-          SizedBox(height: 15),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              children: [
-                Text('Hey Halal,', style: TextStyle(fontSize: 19)),
-                Text(
-                  'Good Afternoon',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                ),
-              ],
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  Text('All Categories', style: TextStyle(fontSize: 25)),
+                  Row(
+                    children: [
+                      Text('See All', style: TextStyle(fontSize: 20)),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.black38,
+                        size: 15,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 15),
-          Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
                 child: Container(
-                  width: 300,
-                  height: 65,
-                  decoration: BoxDecoration(
-                    color: Color(0xffe9eef6),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                  height: 70,
                   child: Row(
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: Icon(
-                          Icons.search,
-                          size: 35,
-                          color: Color(0xffbec1cf),
-                        ),
+                      Category(
+                        bgColor: 0xffffd27c,
+                        imagePath: 'assets/fire.png',
+                        titel: 'All',
+                        hoverColor: Colors.white,
                       ),
-                      Text(
-                        'Search dishes, restaurants',
-                        style: TextStyle(
-                          color: Color(0xffbec1cf),
-                          fontSize: 17,
-                        ),
+                      SizedBox(width: 10),
+                      Category(
+                        bgColor: 0xffffffff,
+                        imagePath: 'assets/hotDog.png',
+                        titel: 'Hot Dog',
+                        hoverColor: Color(0xFFf5f5f4),
+                      ),
+                      SizedBox(width: 10),
+                      Category(
+                        bgColor: 0xffffffff,
+                        imagePath: 'assets/burger.png',
+                        titel: 'Burger',
+                        hoverColor: Color(0xfff5f5f4),
                       ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 30),
-          Row(
-            children: [
-              Text('All Categories'),
-              Row(children: [Text('See All'), Icon(Icons.arrow_forward_ios)]),
-            ],
-          ),
-          Row(
-            children: [
-              Row(
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: .spaceBetween,
+                children: [
+                  Text('Open Restaurants', style: TextStyle(fontSize: 25)),
+                  Row(
+                    children: [
+                      Text('See All', style: TextStyle(fontSize: 20)),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.black38,
+                        size: 15,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
                 children: [
                   Container(
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(),
-                          child: Image.asset('assets/fire.jpg'),
-                        ),
-                        Text('All'),
-                      ],
+                    width: 330,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(),
-                          child: Image.asset('assets/hotDog.jpg'),
-                        ),
-                        Text('Hot Dog'),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(),
-                    child: Row(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(),
-                          child: Image.asset('assets/burger.png'),
-                        ),
-                        Text('Burger'),
-                      ],
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        'assets/firstPhoto.jpeg',
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ],
               ),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Open Restaurants'),
-              Row(children: [Text('See All'), Icon(Icons.arrow_forward_ios)]),
-            ],
-          ),
-
-          Row(
-            children: [
-              Container(
-                decoration: BoxDecoration(),
-                child: Image.asset('assets/firstPhoto.jpeg'),
-              ),
-            ],
-          ),
-          Row(children: [Text('Rose Garden Restaurant')]),
-          Row(children: [Text('Burger - Chiken - Riche - Wings')]),
-          Row(
-            children: [
-              Row(children: [Icon(Icons.star_border), Text('4.7')]),
-              Row(
-                children: [Icon(Icons.local_shipping_outlined), Text('Free')],
-              ),
-              Row(children: [Icon(Icons.timer), Text('20 min')]),
-              Row(
+            ),
+            SizedBox(height: 5),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
                 children: [
-                  Container(
-                    decoration: BoxDecoration(),
-                    child: Image.asset('assets/secondPhoto.jpeg'),
+                  Text(
+                    'Rose Garden Restaurant',
+                    style: TextStyle(fontSize: 25),
                   ),
                 ],
               ),
-            ],
-          ),
-        ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Text(
+                    'Burger - Chiken - Riche - Wings',
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black.withOpacity(0.5),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.only(left: 15, bottom: 20),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.star_border,
+                        color: MyColors.mainColor,
+                        fontWeight: FontWeight.bold,
+                        size: 30,
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        '4.7',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 30),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.local_shipping_outlined,
+                        color: MyColors.mainColor,
+                        fontWeight: FontWeight.bold,
+                        size: 30,
+                      ),
+                      SizedBox(width: 5),
+                      Text('Free', style: TextStyle(fontSize: 20)),
+                    ],
+                  ),
+                  SizedBox(width: 30),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.timer,
+                        color: MyColors.mainColor,
+                        size: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      SizedBox(width: 5),
+                      Text('20 min', style: TextStyle(fontSize: 18)),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Row(
+                children: [
+                  Container(
+                    width: 330,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.asset(
+                        'assets/secondPhoto.jpeg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
